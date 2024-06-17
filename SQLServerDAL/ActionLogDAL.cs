7ebd1f83cd353a71237849190de9ed5a55903404
盛雄峰 <zsxfbj@gmail.com>
2024-06-17 10:07:22 +0800

    优化代码

--- SQLServerDAL/ActionLogDAL.cs
+++ SQLServerDAL/ActionLogDAL.cs
@@ -29,27 +29,26 @@ namespace HC.SQLServerDAL
         public void Insert(ActionLog entity)
         {
             SqlParameter[] parms;
-            if(ParamsCache.GetCachedParameterSet(Database.CONN_STRING_NON_DTC, "SQL_INSERT_ACTION_LOG", out parms))
+            if (ParamsCache.GetCachedParameterSet(Database.CONN_STRING_NON_DTC, "SQL_INSERT_ACTION_LOG", out parms))
             {
                 Database.AssignParameterValues(parms, entity.RequestId, entity.Step, entity.ActionName, entity.RequestData, entity.ResponseData);
             }
             else
             {
-                parms = new SqlParameter[5];
-                parms[0] = new SqlParameter(PARM_REQUEST_ID, System.Data.SqlDbType.VarChar,128);
+                parms = new SqlParameter[] {
+                    new SqlParameter(PARM_REQUEST_ID, SqlDbType.VarChar,128),
+                    new SqlParameter(PARM_STEP, SqlDbType.Int),
+                    new SqlParameter(PARM_ACTION_NAME, SqlDbType.VarChar, 32),
+                    new SqlParameter(PARM_REQUEST_DATA, SqlDbType.NText),
+                    new SqlParameter(PARM_RESPONSE_DATE, SqlDbType.NText)
+                };
+
                 parms[0].Value = entity.RequestId;
-
-                parms[1] = new SqlParameter(PARM_STEP, System.Data.SqlDbType.Int);
                 parms[1].Value = entity.Step;
-
-                parms[2] = new SqlParameter(PARM_ACTION_NAME, System.Data.SqlDbType.VarChar, 32);
                 parms[2].Value = entity.ActionName;
-
-                parms[3] = new SqlParameter(PARM_REQUEST_DATA, System.Data.SqlDbType.NText);
                 parms[3].Value = entity.RequestData;
-
-                parms[4] = new SqlParameter(PARM_RESPONSE_DATE, System.Data.SqlDbType.NText);
                 parms[4].Value = entity.ResponseData;
+
                 ParamsCache.CacheParameterSet(Database.CONN_STRING_NON_DTC, "SQL_INSERT_ACTION_LOG", parms);
             }
             Database.ExecuteNonQuery(Database.CONN_STRING_NON_DTC, CommandType.Text, SQL_INSERT_ACTION_LOG, parms);
